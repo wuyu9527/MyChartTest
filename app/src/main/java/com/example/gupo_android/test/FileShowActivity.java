cabf302de3e65e46a6366b2048052b60b2a17d68
wuhx <546446588@qq.com>
2018-11-27 10:03:36 +0800

    指纹

---
+++ app/src/main/java/com/example/gupo_android/test/FileShowActivity.java
@@ -0,0 +1,195 @@
+package com.example.gupo_android.test;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import es.voghdev.pdfviewpager.library.RemotePDFViewPager;
+import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter;
+import es.voghdev.pdfviewpager.library.remote.DownloadFile;
+
+
+/**
+ * @author : whx
+ * @date : 2018/11/13 17:44
+ */
+
+/**
+ * 文件预览页
+ */
+public class FileShowActivity extends AppCompatActivity implements DownloadFile.Listener {
+
+
+    RelativeLayout content;
+    LinearLayout pdfcontent;
+    private WebView webView;
+    private String urlString;
+    private ProgressBar progressBar;
+    private LinearLayout linearLayout;
+    //微软在线预览office文档
+    private final String MicrosoftOnlinePreviewUrl = "https://view.officeapps.live.com/op/view.aspx?src=";
+    //谷歌在线预览——不推荐使用
+    private final String GoogleOnlinePreviewUrl = "http://docs.google.com/gview?embedded=true&url=";
+    private RemotePDFViewPager remotePDFViewPager;
+    private PDFPagerAdapter adapter;
+    private String fileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_show);
+        content = findViewById(R.id.content);
+        pdfcontent = findViewById(R.id.pdfcontent);
+
+        initData();
+        initView();
+    }
+
+
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        Intent intent = this.getIntent();
+        if (intent == null) return;
+        urlString = intent.getStringExtra("data");//接收上一个页面传过来的文件地址
+        fileName = urlString.substring(urlString.lastIndexOf("/") + 1);//取文件名
+
+    }
+
+    private void initView() {
+        linearLayout =  findViewById(R.id.linear);
+        progressBar =  findViewById(R.id.progressbar);
+        progressBar.setMax(100);
+        webView =  findViewById(R.id.webview);
+
+        if (urlString.endsWith("pdf")) {//是pdf文件
+            checkOsVersion();
+        } else if (isPicture(urlString)) {//是图片，webview可以直接加载网络图片
+
+            webView.loadUrl( urlString);
+        }else {//是office文档，使用 微软在线预览地址+文件地址 预览，Google在线预览需会存在问题
+            webView.loadUrl(MicrosoftOnlinePreviewUrl  + urlString);
+        }
+
+
+        webView.setWebViewClient(new MyWebViewClient());    //设置在本页显示
+        webView.setWebChromeClient(new MyWebChromeClient());  //设置进度监听，自定义类MyWebChromeClient继承自WebChromeClient
+        webView.getSettings().setLoadWithOverviewMode(true);  //设置加载在本页
+        webView.getSettings().setJavaScriptEnabled(true);   //加载javascript
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+    }
+
+    /**
+     * 验证版本号，5.0以上可以预览pdf,否则...
+     * 使用RemotePDFViewPager需添加依赖：【支持5.0以上Android系统】
+     * compile 'es.voghdev.pdfviewpager:library:1.0.1'
+     */
+    private void checkOsVersion() {
+        webView.setVisibility(View.GONE);
+
+
+            /**
+             * 这里添加了pdf文件加载事件
+             * Tools.getServerPath(this) + urlString  是pdf的网络地址
+             */
+            remotePDFViewPager = new RemotePDFViewPager(this,  urlString, this);
+
+    }
+
+    /**
+     * 判断是不是office文档
+     */
+    private boolean isPicture(String filePath) {
+        if (filePath.endsWith("doc") || filePath.endsWith("docx") ||
+                filePath.endsWith("xls") || filePath.endsWith("xlsx") ||
+                filePath.endsWith("ppt") || filePath.endsWith("pptx"))
+            return false;
+        return true;
+    }
+
+
+
+    /**
+     * pdf预览下载事件
+     * 加载成功
+     *
+     * @param url
+     * @param destinationPath
+     */
+    @Override
+    public void onSuccess(String url, String destinationPath) {
+        content.setVisibility(View.GONE);
+        pdfcontent.setVisibility(View.VISIBLE);
+        adapter = new PDFPagerAdapter(this, fileName);
+        remotePDFViewPager.setAdapter(adapter);
+        pdfcontent.addView(remotePDFViewPager);
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+        content.setVisibility(View.GONE);
+        pdfcontent.setVisibility(View.VISIBLE);
+
+        Toast.makeText(this, "文件预览失败", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 加载pdf进度
+     *
+     * @param progress 当前进度——字节数
+     * @param total    总进度——字节数
+     */
+    @Override
+    public void onProgressUpdate(int progress, int total) {
+        progressBar.setProgress((int) (((float) progress) / total * 100));
+    }
+
+
+    //webview页面加载进度监听类
+    class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            //加载进度newProgress
+            progressBar.setProgress(newProgress);
+            if (progressBar.getProgress() > 80) {
+                webView.setVisibility(View.VISIBLE);
+            }
+            if (progressBar.getProgress() >= 100) {
+                linearLayout.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    //页面加载监听类
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);//本webView加载【不会去调用系统的加载】
+            return true;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
+
+
--- app/src/main/java/com/example/gupo_android/test/MainActivity.java
+++ app/src/main/java/com/example/gupo_android/test/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.media.ImageWriter;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -22,8 +24,17 @@ import com.example.gupo_android.test.View.ChartData;
 import com.example.gupo_android.test.View.LineChart;
 import com.example.gupo_android.test.lib.biometriclib.BiometricPromptManager;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -232,8 +243,100 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     });
                 }
                 break;
+            case R.id.button5:
+
+                break;
         }
     }
+
+    /*public static Object parsePdf(File pdfFile) {
+        PDDocument document = null;
+        try {
+            long start = System.currentTimeMillis();
+            document = PDDocument.load(pdfFile);
+            if (document == null) {
+                return null;
+            }
+            int size = document.getNumberOfPages();
+            BufferedImage image = null;
+            FileOutputStream out = null;
+            for (int i = 0; i < size; i++) {
+                image = new PDFRenderer(document).renderImageWithDPI(i, 130, ImageType.RGB);
+                out = new FileOutputStream("D://12//" + i + ".jpg");
+                ImageIO.write(image, "jpg", out);
+                out.close();
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("解析成功耗时：" + (end - start) + "ms");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(document != null){
+                    document.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return document;
+    }
+
+
+    *//**
+     * 将宽度相同的图片，竖向追加在一起 ##注意：宽度必须相同,纵向处理图片
+     *
+     * @param piclist 文件流数组
+     * @param outPath 输出路径
+     *//*
+    public static void y2Pic(List<BufferedImage> piclist, String outPath) {
+        if (piclist == null || piclist.size() <= 0) {
+            System.out.println("图片数组为空!");
+            return;
+        }
+        try {
+            int height = 0, // 总高度
+                    width = 0, // 总宽度
+                    _height = 0, // 临时的高度 , 或保存偏移高度
+                    __height = 0, // 临时的高度，主要保存每个高度
+                    picNum = piclist.size();// 图片的数量
+            File fileImg = null; // 保存读取出的图片
+            int[] heightArray = new int[picNum]; // 保存每个文件的高度
+            BufferedImage buffer = null; // 保存图片流
+            List<int[]> imgRGB = new ArrayList<int[]>(); // 保存所有的图片的RGB
+            int[] _imgRGB; // 保存一张图片中的RGB数据
+            for (int i = 0; i < picNum; i++) {
+                buffer = piclist.get(i);
+                heightArray[i] = _height = buffer.getHeight();// 图片高度
+                if (i == 0) {
+                    width = buffer.getWidth();// 图片宽度
+                }
+                height += _height; // 获取总高度
+                _imgRGB = new int[width * _height];// 从图片中读取RGB
+                _imgRGB = buffer.getRGB(0, 0, width, _height, _imgRGB, 0, width);
+                imgRGB.add(_imgRGB);
+            }
+            _height = 0; // 设置偏移高度为0
+            // 生成新图片
+            BufferedImage imageResult = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
+            for (int i = 0; i < picNum; i++) {
+                __height = heightArray[i];
+                if (i != 0)
+                    _height += __height; // 计算偏移高度
+                imageResult.setRGB(0, _height, width, __height, imgRGB.get(i), 0, width); // 写入流中
+            }
+            File outFile = new File(outPath);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(imageResult, "jpg", out);// 写图片
+            byte[] b = out.toByteArray();
+            FileOutputStream output = new FileOutputStream(outFile);
+            output.write(b);
+            out.close();
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 
     @Override
     protected void onDestroy() {
